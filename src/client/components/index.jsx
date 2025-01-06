6d2b06bedae04174eff95adb027845c3203a63ff
Najeeb Hazem <164892424+HerculesHazem@users.noreply.github.com>
2025-01-06 13:40:23 +0100

    Revert "Fix/nodejs"

--- src/client/components/index.jsx
+++
@@ -1,6 +0,0 @@
-export { Header } from "./Header";
-export { UserContextProvider } from "./UserContext";
-export { UserContext } from "./UserContext";
-export { HouseOfThrones } from "./HousesOfThrones";
-export { Loader } from "./Loader";
-export { MapGrid } from "./MapGrid";
--- src/client/pages/index.jsx
+++
@@ -1,4 +0,0 @@
-export { GamePage } from "./GamePage";
-export { LoginPage } from "./LoginPage";
-export { RegisterPage } from "./RegisterPage";
-export { HomePage } from "./HomePage";
